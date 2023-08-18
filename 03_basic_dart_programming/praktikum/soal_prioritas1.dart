ce46b47165fcf44a903f163daec1ed9a73f97b10
iqbalgany <rismagany1203@gmail.com>
2023-08-18 23:05:59 +0700

    submit tugas fundamental dart

---
+++ 03_basic_dart_programming/praktikum/soal_prioritas1.dart
@@ -0,0 +1,17 @@
+void main() {
+  // Soal Prioritas 1 No 1
+  int s = 5;
+  int kelilingPersegi = s * 4;
+  int luasPersegi = s * s;
+
+  print('Keliling Persegi : ${kelilingPersegi}');
+  print('Luas Persegi : ${luasPersegi}\n');
+
+  // Soal Prioritas 1 No 2
+  int p = 3;
+  int l = 4;
+  int kelilingPersegiPanjang = 2 * (p + l);
+  int luasPersegiPanjang = p * l;
+  print('Keliling Persegi Panjang : ${kelilingPersegiPanjang}');
+  print('Luas Persegi Panjang : ${luasPersegiPanjang}');
+}
---
+++ 03_basic_dart_programming/praktikum/soal_prioritas2.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Soal Prioritas 2 No 1
+  String namaAwal = 'Muhammad';
+  String namaTengah = 'Iqbal';
+  String namaAkhir = 'Al Afgany';
+
+  print('Nama Lengkap : ${namaAwal} ${namaTengah} ${namaAkhir}\n');
+
+  // Soal Prioritas 2 No 2
+  double phi = 3.14;
+  int r = 6;
+  int t = 10;
+  double volumeTabung = phi * r * r * t;
+
+  print('Volume Tabung : ${volumeTabung}');
+}
