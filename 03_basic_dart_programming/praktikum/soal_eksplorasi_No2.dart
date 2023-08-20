0147b6b63996b58aa8a0c663d673327ff791de11
iqbalgany <rismagany1203@gmail.com>
2023-08-20 07:46:58 +0700

    update

--- 03_basic_dart_programming/praktikum/soal_eksplorasi_No2.dart
+++ 03_basic_dart_programming/praktikum/soal_eksplorasi_No2.dart
@@ -1,7 +1,7 @@
 void main() {
   int bil = 24;
-  print("Faktor dari 24 :");
   int tambah = 0;
+  print("Faktor dari 24 :");
   for (int i = 0; i <= bil; i++) {
     tambah++;
     if (bil % tambah == 0) {
