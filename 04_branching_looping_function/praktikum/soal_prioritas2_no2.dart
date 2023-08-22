839de6054403002de85c3c4dda52eefd277ca510
iqbalgany <rismagany1203@gmail.com>
2023-08-22 20:52:43 +0700

    upload tugas branching, looping, function

---
+++ 04_branching_looping_function/praktikum/soal_prioritas2_no2.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Masukkan sebuah karakter : ');
+  String karakter = stdin.readLineSync().toString();
+  stdout.write('Masukkan nilai : ');
+  int nilai = int.parse(stdin.readLineSync().toString());
+
+  for (int a = 0; a <= (nilai - 1); a++) {
+    for (int b = 1; b <= a; b++) {
+      stdout.write(' ');
+    }
+    for (int c = 0; c >= (a - nilai + 1); c--) {
+      stdout.write(karakter + ' ');
+    }
+    stdout.write('\n');
+  }
+
+  for (int i = 0; i < nilai; i++) {
+    for (int j = 1; j < nilai - i; j++) {
+      stdout.write(' ');
+    }
+    for (int k = 0; k < (i + 1); k++) {
+      stdout.write(karakter + ' ');
+    }
+    stdout.write('\n');
+  }
+}
---
+++ 04_branching_looping_function/praktikum/soal_prioritas2_no4.dart
@@ -0,0 +1,9 @@
+void main() {
+  luasLingkaran(4);
+}
+
+void luasLingkaran(int r) {
+  double phi = 3.14;
+  double luasLingkaran = phi * r * r;
+  print('Luas Lingkaran adalah : ${luasLingkaran}');
+}
