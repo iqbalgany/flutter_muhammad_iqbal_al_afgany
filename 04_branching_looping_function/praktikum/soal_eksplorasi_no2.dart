3c7a199ec72e4a532378f9f71cc0ab3f7c9bd8e9
iqbalgany <rismagany1203@gmail.com>
2023-08-23 18:59:19 +0700

    update tugas

--- 04_branching_looping_function/praktikum/soal_eksplorasi_no2.dart
+++ 04_branching_looping_function/praktikum/soal_eksplorasi_no2.dart
@@ -1,22 +1,15 @@
 import 'dart:io';
 
 void main() {
-  int baris, kolom, n, x;
+  int baris, kolom, n;
 
   stdout.write('Masukkan Ordo : ');
   n = int.parse(stdin.readLineSync().toString());
-
-  // stdout.write(' X ');
-  // for (x = 1; x <= n; x++) {
-  //   stdout.write(' $x ');
-  // }
-
   stdout.write('\n');
 
   for (baris = 1; baris <= n; baris++) {
-    stdout.write(' $baris');
     for (kolom = 1; kolom <= n; kolom++) {
-      stdout.write('  ${baris * kolom}');
+      stdout.write('\t${baris * kolom}');
     }
     stdout.write('\n');
   }
